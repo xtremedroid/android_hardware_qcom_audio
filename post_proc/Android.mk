8faccea79221e88f831ee8e989661583448f92d8
XO <jyotiraditya182@gmail.com>
2018-07-12 10:11:36 -0700

    Update Android.mk

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -108,12 +108,12 @@ LOCAL_MODULE:= libvolumelistener
 LOCAL_VENDOR_MODULE := true
 
 LOCAL_C_INCLUDES := \
-        hardware/qcom/audio/hal \
+        hardware/qcom/audio-caf-msm8996/hal \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
         external/tinyalsa/include \
         $(call include-path-for, audio-effects) \
         $(call include-path-for, audio-route) \
-        hardware/qcom/audio/hal/audio_extn \
+        hardware/qcom/audio-caf-msm8996/hal/audio_extn \
         external/tinycompress/include
 
 LOCAL_C_INCLUDES	+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
