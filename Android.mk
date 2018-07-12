af41a0c79a3da596c133770681d76ab882644333
XO <jyotiraditya182@gmail.com>
2018-07-12 10:08:40 -0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,6 @@
-ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msm8998 sdm660 sdm845 apq8098_latv,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-msm8996)
+
+ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msmcobalt msmfalcon,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 
